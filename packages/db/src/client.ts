a59d44f7c170a847de296c82d4ddabcdff9ee660
purvjoshi <101319136+purvjoshi04@users.noreply.github.com>
2025-11-27 16:09:00 +0530

    fix: prisma new version issue

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,12 +1,7 @@
-import { PrismaClient } from "./generated/prisma/client"
-import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/client/index.js';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-});
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
