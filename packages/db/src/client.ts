4ebc99082d6a2bdfab32ed8d0a6e8a003f735041
purvjoshi <101319136+purvjoshi04@users.noreply.github.com>
2026-02-18 23:04:31 +0530

    feat: updated prisma package to 7 version

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from '../generated/client/index.js';
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({ adapter });
